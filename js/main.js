4b2f268e55a50db3ecf2fcc1bb21f8c91449368d
Lucas McCallum <lucasmccallum@lucass-mbp.fc.ca>
2017-03-08 13:06:34 -0500

    localstorage todolist

---
+++ js/main.js
@@ -0,0 +1,28 @@
+(function(){
+var list = document.querySelector('#list'),
+saveAll = document.querySelector("#saveAll"),
+clearAll = document.querySelector('#clearAll');
+
+
+function emptyStorage(e){
+  localStorage.clear();
+}
+
+function fillStorage(){
+  localStorage.setItem( 'todoList', list.innerHTML);
+}
+
+function loadToDo(){
+  if(window.localStorage){
+    if(localStorage.getItem('todoList')) {
+      list.innerHTML = localStorage.getItem('todoList');
+    };
+  } else {
+    console.log('get a better browser!');
+  }
+}
+
+clearAll.addEventListener('click', emptyStorage, false);
+saveAll.addEventListener('click', fillStorage, false);
+loadToDo();
+})();
